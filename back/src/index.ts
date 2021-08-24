59e5bb12cda22c4ee5eb1af8da98beb5788615ab
Alfonsina Lizardo <alfonsina.l@gmail.com>
2021-08-23 21:45:29 -0400

    refactor: undo refactor of index file feat: add frontend code to public

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,6 +1,22 @@
-import Server from './services/server';
+import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import routes from './routes';
+import { unknownEndpoint } from './middlewares/unknownEndpoint';
 
+const app: express.Application = express();
 const PORT = process.env.PORT || 8080;
 
-Server.listen(PORT, () => console.log(`Servidor inicializado en http://localhost:${PORT}`));
-Server.on('error', (error) => console.log(`Error en el servidor: ${error}`));
+const server = app.listen(PORT, () => {
+  console.log(`Servidor inicializado en http://localhost:${PORT}`);
+});
+server.on('error', (error) => console.log(`Error en el servidor: ${error}`));
+
+app.use(express.static(path.resolve(__dirname, '../', 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.use('/api', routes);
+
+app.use(unknownEndpoint);
