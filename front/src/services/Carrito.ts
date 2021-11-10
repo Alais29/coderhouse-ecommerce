ef58326eb17557f43309b5924f8aa47b50ab2b2d
Alfonsina Lizardo <alfonsina.l@gmail.com>
2021-11-09 22:36:26 -0300

    chore: updaturl for deleting product from cart

--- front/src/services/Carrito.ts
+++ front/src/services/Carrito.ts
@@ -1,30 +1,30 @@
-import axios from "axios";
+import axios from 'axios';
 
-const baseUrl = "/api/carrito";
+const baseUrl = '/api/carrito';
 
 export const getCarritoProducts = async () => {
   try {
     const response = await axios.get(`${baseUrl}/listar`);
-    return response.data.data
+    return response.data.data;
   } catch (e) {
-    throw new Error(e.response.data.message)
+    throw new Error(e.response.data.message);
   }
-}
+};
 
 export const saveCarritoProduct = async (id: string) => {
   try {
-    const response = await axios.post(`${baseUrl}/agregar/${id}`)
-    return response.data.data
+    const response = await axios.post(`${baseUrl}/agregar/${id}`);
+    return response.data.data;
   } catch (e) {
     throw new Error(e.response.data.message);
   }
-}
+};
 
 export const deleteCarritoProduct = async (id: string) => {
   try {
-    const response = await axios.delete(`${baseUrl}/borrar/${id}`)
-    return response.data.data
+    const response = await axios.delete(`${baseUrl}/eliminar/${id}`);
+    return response.data.data;
   } catch (e) {
     throw new Error(e.response.data.message);
   }
-}
+};
