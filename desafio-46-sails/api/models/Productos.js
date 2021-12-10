8db992f48fc082ab282599e881562246d97f26e3
Alfonsina Lizardo <sstanich@aaxiscommerce.com>
2021-12-10 13:43:10 -0300

    feat: desafio-46 done

---
+++ desafio-46-sails/api/models/Productos.js
@@ -0,0 +1,39 @@
+/**
+ * Productos.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    id: { type: 'string', columnName: '_id' },
+    nombre: { type: 'string', required: true, columnName: 'nombre'},
+    descripcion: { type: 'string', required: true, columnName: 'descripcion'},
+    codigo: { type: 'string', required: true, columnName: 'codigo'},
+    precio: { type: 'number', required: true, columnName: 'precio'},
+    categoria: { type: 'string', required: true, columnName: 'categoria'},
+    foto: { type: 'string', required: true, columnName: 'foto'},
+    stock: { type: 'number', required: true, columnName: 'stock' },
+    createdAt: { type: 'number', autoCreatedAt: true, },
+    updatedAt: { type: 'number', autoUpdatedAt: true, },
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+  },
+
+};
+
