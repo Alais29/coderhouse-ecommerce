a16c33d0cbe3fdaa100a391bfcbb496dbef23b8d
Alfonsina Lizardo <alfonsina.l@gmail.com>
2022-01-26 17:26:57 -0300

    refactor: edit meta tags chore: update public folder in back

--- front/src/components/Helmet/Helmet.tsx
+++ front/src/components/Helmet/Helmet.tsx
@@ -5,7 +5,7 @@ const Helmet = () => {
     <ReactHelmet>
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta name="theme-color" content="#000000" />
+      <meta name="theme-color" content="#1a4a4f" />
       <link
         rel="icon"
         type="image/png"
@@ -14,20 +14,22 @@ const Helmet = () => {
       />
       <title>MERN Ecommerce</title>
       <meta name="author" content="Alfonsina Lizardo" />
-      <meta property="og:type" content="website" />
-      <meta property="og:site_name" content="MERN Ecommerce" />
       <meta
         name="description"
         content="A fullstack MERN ecommerce application built in Typescript. Includes several features such as login/signup, cart, orders, chat with websockets, among others."
       />
-      <meta name="title" property="og:title" content="MERN Ecommerce" />
+      <meta property="og:type" content="website" />
+      <meta property="og:site_name" content="MERN Ecommerce" />
+      <meta property="og:title" content="MERN Ecommerce" />
       <meta
-        name="description"
         property="og:description"
         content="A fullstack MERN ecommerce application built in Typescript. Includes several features such as login/signup, cart, orders, chat with websockets, among others."
       />
       <meta
-        name="image"
+        property="og:url"
+        content="https://mern-ecommerce-coder.herokuapp.com/#/"
+      />
+      <meta
         property="og:image"
         content="https://res.cloudinary.com/alais29/image/upload/v1643209711/ogImage_ekmmnq.jpg"
       />
@@ -35,6 +37,7 @@ const Helmet = () => {
       <meta property="og:image:type" content="image/jpeg" />
       <meta property="og:image:width" content="1048" />
       <meta property="og:image:height" content="936" />
+      <meta name="twitter:card" content="summary_large_image" />
     </ReactHelmet>
   );
 };
